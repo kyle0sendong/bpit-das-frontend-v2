58ba9bff0cd22261f285b774b76f990406871a44
Kyle Destura <kyle.afundar@gmail.com>
2025-01-09 15:00:13 +0800

    Added navbar on configurations

---
+++ src/pages/configurations/components/navbar/NavbarNested.tsx
@@ -0,0 +1,48 @@
+import {
+  IconAdjustments,
+  IconCalendarStats,
+  IconFileAnalytics,
+  IconGauge,
+  IconLock,
+  IconNotes,
+  IconPresentationAnalytics,
+} from '@tabler/icons-react';
+import { ScrollArea } from '@mantine/core';
+import { LinksGroup } from "../links-group/NavbarLinksGroup"
+import classes from './NavbarNested.module.css';
+
+const mockdata = [
+  { label: 'Station', icon: IconGauge },
+  { label: 'Virtual Channels', icon: IconFileAnalytics },
+  {
+    label: 'TCP Analyzers',
+    icon: IconNotes,
+    links: [
+      { label: 'Overview', link: '/' },
+      { label: 'Forecasts', link: '/' },
+      { label: 'Outlook', link: '/' },
+      { label: 'Real time', link: '/' },
+    ],
+  },
+  {
+    label: 'Serial Analyzers',
+    icon: IconCalendarStats,
+    links: [
+      { label: 'Upcoming releases', link: '/' },
+      { label: 'Previous releases', link: '/' },
+      { label: 'Releases schedule', link: '/' },
+    ],
+  }
+];
+
+export default function NavbarNested() {
+  const links = mockdata.map((item) => <LinksGroup {...item} key={item.label} />);
+
+  return (
+    <nav className={classes.navbar}>
+      <ScrollArea className={classes.links}>
+        <div className={classes.linksInner}>{links}</div>
+      </ScrollArea>
+    </nav>
+  );
+}
