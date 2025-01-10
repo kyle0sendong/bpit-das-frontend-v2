9466001779108845bdddb76690655d137b281caa
Kyle Destura <kyle.afundar@gmail.com>
2025-01-10 14:25:10 +0800

    Added tcp cards

--- src/pages/configurations/pages/tcp-analyzers/TcpAnalyzers.tsx
+++ src/pages/configurations/pages/tcp-analyzers/TcpAnalyzers.tsx
@@ -1,8 +1,28 @@
+import { Box, Flex } from "@mantine/core";
+import { useSearchParams } from "react-router-dom";
+
+import { useGetParametersByAnalyzerId } from "@/hooks/parametersHook";
+
+import TcpAnalyzerCard from "./components/TcpAnalyzerCard"
+
 const TcpAnalyzers = () => {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("id") ?? '0';
+
+  const parameters = useGetParametersByAnalyzerId(parseInt(id));
+
+
+  if(parameters.isFetched) {
+    console.log(parameters.data)
+  }
+
   return (
-    <div>
-      TCP Analyzers
-    </div>
+    <Box w="100%">
+      <Flex h="10%">
+        <TcpAnalyzerCard id={id}/>
+      </Flex>
+      <Flex h="90%" w="100%"></Flex>
+    </Box>
   )
 }
 
---
+++ src/pages/configurations/pages/tcp-analyzers/components/TcpAnalyzerCard.tsx
@@ -0,0 +1,51 @@
+import { Loader, Paper, Button, Flex, Box, Text } from "@mantine/core";
+import { useGetTcpAnalyzerById } from "@/hooks/tcpAnalyzersHook";
+
+import { TcpAnalyzerType } from "@/types/tcpAnalyzers";
+
+const TcpAnalyzerCard = ({id}: {id: string}) => {
+
+  const tcpAnalyzer = useGetTcpAnalyzerById(parseInt(id));
+
+  if(tcpAnalyzer.isFetched) {
+
+    const tcpAnalyzerData: TcpAnalyzerType = tcpAnalyzer.data[0];
+
+    console.log(tcpAnalyzerData)
+    return (
+      <Flex w="100%" m="xs">
+
+        <Paper shadow="md" my="auto" p="xs">
+          <Flex gap={14} align="center">
+            <Paper shadow="md">
+              <Button h="100%" p="lg" variant="default" style={{fontSize:"1.3rem"}}>
+                {tcpAnalyzerData.name}
+              </Button>
+            </Paper>
+
+            <Box px="sm">
+              <Text my="xs" size="0.9rem"> <b>IP Address:</b> {tcpAnalyzerData.host_address}</Text>
+              <Text my="xs" size="0.9rem"> <b>Port:</b> {tcpAnalyzerData.port}</Text>
+            </Box>
+
+            <Box px="sm">
+              <Text my="xs" size="0.9rem"> <b>Device Address:</b> {tcpAnalyzerData.device_address}</Text>
+              <Text my="xs" size="0.9rem"> <b>Data Threshold:</b> {tcpAnalyzerData.sampling}%</Text>
+            </Box>
+          </Flex>
+
+        </Paper>
+      </Flex>
+
+    )
+  }
+
+  if(tcpAnalyzer.isLoading) {
+    return (
+      <Loader size="lg"/>
+    )
+  }
+
+}
+
+export default TcpAnalyzerCard;
