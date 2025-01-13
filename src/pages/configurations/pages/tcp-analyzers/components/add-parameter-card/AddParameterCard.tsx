f0b8f6ff97cb2310a25e269516736b5fbe93e350
Kyle Destura <kyle.afundar@gmail.com>
2025-01-13 13:30:58 +0800

    Added insert parameter card

---
+++ src/pages/configurations/pages/tcp-analyzers/components/add-parameter-card/AddParameterCard.tsx
@@ -0,0 +1,44 @@
+import { Button, NumberInput, Flex } from "@mantine/core";
+import { useInsertParameter } from "@/hooks/parametersHook";
+import { useForm } from "@mantine/form";
+import { InsertParameterType } from "@/types/parameters";
+
+
+const AddParameterCard = ({id}: {id: string}) => {
+
+  const form = useForm<InsertParameterType>({
+    mode:"uncontrolled",
+    initialValues: {
+      number: 1,
+      id: parseInt(id),
+      name: "Default"
+    }
+  });
+  
+  const { mutate: insertParameter } = useInsertParameter(parseInt(id));
+
+  return (
+    <form onSubmit={
+      form.onSubmit( (value) => {
+
+        form.reset();
+        console.log(value);
+        insertParameter(value);
+      })
+    }>
+      <Flex align="center" p="xs" gap="md">
+          <NumberInput   
+            size="xs"
+            label="Insert Parameters"
+            description="Number of parameters to be inserted"
+            key={form.key('number')}
+            {...form.getInputProps('number')}
+          />
+          <Button variant="outline" type="submit"> Save</Button>
+      </Flex>
+    </form>
+
+  )
+}
+
+export default AddParameterCard;
