bc2bd14a33c438004d7f0afb630978d3113f5a2f
Kyle Destura <kyle.afundar@gmail.com>
2025-01-14 11:30:29 +0800

    Added virtual channel card with insert

---
+++ src/pages/configurations/pages/virtual-channels/components/add-virtual-channel-card/AddVirtualChannelCard.tsx
@@ -0,0 +1,41 @@
+import { Button, NumberInput, Flex } from "@mantine/core";
+import { useForm } from "@mantine/form";
+
+import { useInsertVirtualChannel } from "@/hooks/virtualChannelsHook";
+
+import { InsertVirtualChannelType } from "@/types/virtualChannels";
+
+const AddVirtualChannelCard = () => {
+
+  const form = useForm<InsertVirtualChannelType>({
+    mode:"uncontrolled",
+    initialValues: {
+      number: 1
+    }
+  });
+  
+  const { mutate: insertVirtualChannel } = useInsertVirtualChannel();
+
+  return (
+    <form onSubmit={
+      form.onSubmit( (value) => {
+        form.reset();
+        insertVirtualChannel(value);
+      })
+    }>
+      <Flex align="center" p="xs" gap="md">
+        <NumberInput   
+          size="xs"
+          label="Insert Virtual Channel"
+          description="Number of Virtual Channels to be inserted"
+          key={form.key('number')}
+          {...form.getInputProps('number')}
+        />
+        <Button variant="outline" type="submit"> Save</Button>
+      </Flex>
+    </form>
+
+  )
+}
+
+export default AddVirtualChannelCard;
