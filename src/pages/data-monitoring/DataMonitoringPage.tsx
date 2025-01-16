ffe28c265bd8e4a02068460c9b1b0fd89ad628e8
Kyle Destura <kyle.afundar@gmail.com>
2025-01-16 14:13:16 +0800

    Add sidebar on data monitroing

---
+++ src/pages/data-monitoring/DataMonitoringPage.tsx
@@ -0,0 +1,14 @@
+import { Flex } from "@mantine/core";
+import SidebarMenu from "./components/sidebar-menu/SidebarMenu";
+
+const DataMonitoringPage = () => {
+  return (
+    <Flex w="100%" bd="1px solid red">
+
+      <SidebarMenu />
+      Data Monitoring
+    </Flex>
+  )
+}
+
+export default DataMonitoringPage;
