246531ee90c85373b10eb86b42f2e17bfacef247
Kyle Destura <kyle.afundar@gmail.com>
2025-01-16 16:02:17 +0800

    Make analyzer picker functional

--- src/pages/data-history/components/sidebar-menu/components/AnalyzerPicker.tsx
+++ src/pages/data-history/components/sidebar-menu/components/AnalyzerPicker.tsx
@@ -1,21 +1,44 @@
-import { Flex, NativeSelect } from "@mantine/core";
+import { Flex, NativeSelect, Loader } from "@mantine/core";
 import { UseFormReturnType } from "@mantine/form";
 import { FormSubmitType } from "../SidebarMenu";
+
+import { useGetAllParameters } from "@/hooks/parametersHook";
+import { ParametersType } from "@/types/parameters";
 
 type TableRowsProps = {
   form: UseFormReturnType<Partial<FormSubmitType>>;
 }
 
 const AnalyzerPicker = ({form}: TableRowsProps ) => {
-  return (
-    <Flex direction="column" mx="xs" mb="sm">
-      <NativeSelect
-        data={['1 minute', '5 minutes']}
-        key={form.key('analyzer')}
-        {...form.getInputProps('analyzer')}
-      />
-    </Flex>
-  )
+
+  const parameters = useGetAllParameters();
+
+  if(parameters.isLoading) {
+    return (
+      <Loader size="lg" />
+    )
+  }
+
+  if(parameters.isFetched) {
+    const parameterData: ParametersType[] = parameters.data;
+    const dataMenu = parameterData.map( (data) => {
+      return {
+        label: data.name,
+        value: data.id.toString()
+      }
+    })
+
+    return (
+      <Flex direction="column" mx="xs" mb="sm">
+        <NativeSelect
+          data={dataMenu}
+          key={form.key('analyzer')}
+          {...form.getInputProps('analyzer')}
+        />
+      </Flex>
+    )
+  }
+
 }
 
 export default AnalyzerPicker;
