a938ed72fbff19540d554a91598f3f6e4a96a6e4
Kyle Destura <kyle.afundar@gmail.com>
2025-01-21 16:42:06 +0800

    Fix bug on inserting parameters

--- src/pages/configurations/pages/tcp-analyzers/components/add-parameter-card/AddParameterCard.tsx
+++ src/pages/configurations/pages/tcp-analyzers/components/add-parameter-card/AddParameterCard.tsx
@@ -3,7 +3,6 @@ import { useInsertTcpParameter } from "@/hooks/tcpParametersHook";
 import { useForm } from "@mantine/form";
 import { InsertTcpParameterType } from "@/types/tcpParameters";
 
-
 const AddParameterCard = ({id}: {id: string}) => {
 
   const form = useForm<InsertTcpParameterType>({
@@ -20,21 +19,18 @@ const AddParameterCard = ({id}: {id: string}) => {
   return (
     <form onSubmit={
       form.onSubmit( (value) => {
-
-        form.reset();
-        console.log(value);
         insertParameter(value);
       })
     }>
       <Flex align="center" p="xs" gap="md">
-          <NumberInput   
-            size="xs"
-            label="Insert Parameters"
-            description="Number of parameters to be inserted"
-            key={form.key('number')}
-            {...form.getInputProps('number')}
-          />
-          <Button variant="outline" type="submit"> Save</Button>
+        <NumberInput   
+          size="xs"
+          label="Insert Parameters"
+          description="Number of parameters to be inserted"
+          key={form.key('number')}
+          {...form.getInputProps('number')}
+        />
+        <Button variant="outline" type="submit" onClick={() => form.setFieldValue("id", parseInt(id))}> Save</Button>
       </Flex>
     </form>
 
