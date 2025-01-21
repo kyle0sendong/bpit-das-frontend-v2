eea5b2e79c9f316baba0871a735b1a045f81b7ed
Kyle Destura <kyle.afundar@gmail.com>
2025-01-21 10:43:28 +0800

    Added convert date to string utility

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,3 +1,5 @@
+import { DateValue } from "@mantine/dates";
+
 export const getCurrentDate = () => {
   const tempDate = new Date()
   const currentDate = `${tempDate.getFullYear()}-${tempDate.getMonth()+1}-${tempDate.getDate()}`
@@ -14,4 +16,10 @@ export const getCurrentTime = () => {
   const temp = new Date()
   const currentTime = `${temp.getHours()}:${temp.getMinutes()}`
   return currentTime
+}
+
+export const convertDateToString = (date: DateValue) => {
+  const month = date?.getMonth() ?? 1;
+  const convertedDate = `${date?.getFullYear()}-${month + 1}-${date?.getDate()}`
+  return convertedDate;
 }
