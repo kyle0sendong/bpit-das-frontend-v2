96cb85ffbda9faf5f46b5dd796c6cbc965b24fdd
Kyle Destura <kyle.afundar@gmail.com>
2025-02-04 14:10:23 +0800

    Added layout for account settings page

--- src/pages/account/pages/account-settings/AccountSettingsPage.tsx
+++ src/pages/account/pages/account-settings/AccountSettingsPage.tsx
@@ -1,10 +1,48 @@
+import { Flex, Text, TextInput, Paper, Button, Avatar } from "@mantine/core";
 
 const AccountSettingsPage = () => {
 
   return (
-    <>
-      account settings
-    </>
+    <Paper w="50%" m="auto" p="lg">
+
+      <Flex gap="lg" direction="column" flex={1}>
+
+        <Flex m="sm" gap="lg" align="center">
+          <Text miw="6rem"> Profile Picture </Text>
+          <Avatar size="5rem" mr="lg"/>
+          <Button variant="default">Change Profile Picture</Button>
+        </Flex>
+
+        <Flex m="sm" gap="lg" align="center">
+          <Text miw="6rem"> First Name </Text>
+          <TextInput flex="1"/>
+        </Flex>
+        <Flex m="sm" gap="lg" align="center">
+          <Text miw="6rem"> Last Name </Text>
+          <TextInput flex="1"/>
+        </Flex>
+        <Flex m="sm" gap="lg" align="center">
+          <Text miw="6rem"> Username </Text>
+          <TextInput flex="1"/>
+        </Flex>
+        <Flex m="sm" gap="lg" align="center" >
+          <Text miw="6rem"> Email </Text>
+          <TextInput flex="1"/>
+        </Flex>
+        <Flex m="sm" gap="lg" align="center" >
+          <Text miw="6rem"> Password </Text>
+          <Button variant="default"> Change Password</Button>
+        </Flex>
+
+        <Flex justify="flex-end">
+          <Button>
+            Save
+          </Button>
+        </Flex>
+      </Flex>
+
+      
+    </Paper>
   )
 }
 
