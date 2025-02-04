eb3b088fdaa8106826cb7934b1dd18c115aff11e
Kyle Destura <kyle.afundar@gmail.com>
2025-02-04 13:34:10 +0800

    Add users table

--- src/pages/account/pages/user-dashboard/UserDashboardPage.tsx
+++ src/pages/account/pages/user-dashboard/UserDashboardPage.tsx
@@ -1,9 +1,32 @@
+import { Box, Flex, Loader, Button, Paper } from "@mantine/core";
+import UserTable from "./components/UserTable";
+import { useGetAllUsers } from "@/hooks/usersHook";
+
 const UserDashboardPage = () => {
 
+  const users = useGetAllUsers();
+
+  const table = users.isFetched ? 
+    <UserTable data={users.data} /> : 
+    <Loader />;
+
   return (
-    <>
-      User Dashboard Page
-    </>
+    <Paper w="70%" m="auto" p="xs">
+      <Flex direction="column" gap="xs">
+        <Box>
+          <Button>
+            Register a User
+          </Button>
+        </Box>
+
+        <Box>
+          {table}
+        </Box>
+
+      </Flex>
+    </Paper>
+
+
   )
 }
 
