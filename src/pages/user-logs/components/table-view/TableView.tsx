58bc94fd567a6b3e4537bb515251696763493a0c
Kyle Destura <kyle.afundar@gmail.com>
2025-02-05 14:40:37 +0800

    Replaced fallback table with a loader

--- src/pages/user-logs/components/table-view/TableView.tsx
+++ src/pages/user-logs/components/table-view/TableView.tsx
@@ -1,12 +1,6 @@
 import { useSearchParams } from 'react-router-dom';
 
-import {
-  MantineReactTable,
-  useMantineReactTable,
-  type MRT_ColumnDef
-} from 'mantine-react-table';
-
-import { Text } from '@mantine/core';
+import { Text, Loader } from '@mantine/core';
 
 
 import { useGetUserLogsByDate } from '@/hooks/userLogsHook';
@@ -15,27 +9,11 @@ import { getCurrentDate } from '@/utils/dates';
 
 import { UserLogsType } from '@/types/userLogs';
 
-const columns: MRT_ColumnDef<any>[] = [ 
-  {
-    accessorKey: 'datetime',
-    header: 'Date & Time',
-    size: 100
-  }
-]
-
-const data: any[] = []
-
 const TableView = () => {
 
   const [ searchParams ] = useSearchParams()
   const from = searchParams.get("from") ?? getCurrentDate();
   const to = searchParams.get("to") ?? getCurrentDate();
-
-  const table = useMantineReactTable({
-    columns,
-    data, //should fallback to empty array while loading data
-    state: { isLoading: true },
-  });
 
   const userLogs = useGetUserLogsByDate(from, to);
 
@@ -51,7 +29,7 @@ const TableView = () => {
 
   if(userLogs.isLoading) {
     return (
-      <MantineReactTable table={table} />
+      <Loader />
     )
   }
 
