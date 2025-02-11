880731f14d5516d8f43a657017fd0bbf73c0489b
Kyle Destura <kyle.afundar@gmail.com>
2025-02-11 10:51:01 +0800

    Added module css and initialized form element

--- src/pages/account/pages/account-settings/AccountSettingsPage.tsx
+++ src/pages/account/pages/account-settings/AccountSettingsPage.tsx
@@ -1,44 +1,77 @@
-import { Flex, Text, TextInput, Paper, Button, Avatar } from "@mantine/core";
+import { Flex, Text, TextInput, Paper, Button, Avatar } from '@mantine/core';
+import { useForm } from '@mantine/form';
+
+import classes from './AccountSettings.module.css'
 
 const AccountSettingsPage = () => {
 
+  const form = useForm({
+    mode:'uncontrolled'
+  });
+
   return (
-    <Paper w="50%" m="auto" p="lg">
+    <Paper w='50%' m='auto' p='lg'>
 
-      <Flex gap="lg" direction="column" flex={1}>
+      <Flex gap='lg' direction='column' flex={1}>
 
-        <Flex m="sm" gap="lg" align="center">
-          <Text miw="6rem"> Profile Picture </Text>
-          <Avatar size="5rem" mr="lg"/>
-          <Button variant="default">Change Profile Picture</Button>
-        </Flex>
+        <form onSubmit={form.onSubmit((values) => {
+          console.log(values);
+        })}>
 
-        <Flex m="sm" gap="lg" align="center">
-          <Text miw="6rem"> First Name </Text>
-          <TextInput flex="1"/>
-        </Flex>
-        <Flex m="sm" gap="lg" align="center">
-          <Text miw="6rem"> Last Name </Text>
-          <TextInput flex="1"/>
-        </Flex>
-        <Flex m="sm" gap="lg" align="center">
-          <Text miw="6rem"> Username </Text>
-          <TextInput flex="1"/>
-        </Flex>
-        <Flex m="sm" gap="lg" align="center" >
-          <Text miw="6rem"> Email </Text>
-          <TextInput flex="1"/>
-        </Flex>
-        <Flex m="sm" gap="lg" align="center" >
-          <Text miw="6rem"> Password </Text>
-          <Button variant="default"> Change Password</Button>
-        </Flex>
+          <Flex className={classes.flexItem}>
+            <Text className={classes.text}> Profile Picture </Text>
+            <Avatar size='5rem' mr='lg'/>
+            <Button variant='default'>Change Profile Picture</Button>
+          </Flex>
 
-        <Flex justify="flex-end">
-          <Button>
-            Save
-          </Button>
-        </Flex>
+          <Flex className={classes.flexItem}>
+            <Text className={classes.text}> First Name </Text>
+            <TextInput 
+              className={classes.textInput}
+              key={form.key('first_name')}
+              {...form.getInputProps('first_name')}
+            />
+          </Flex>
+
+          <Flex className={classes.flexItem}>
+            <Text className={classes.text}> Last Name </Text>
+            <TextInput 
+              className={classes.textInput}
+              key={form.key('last_name')}
+              {...form.getInputProps('last_name')}
+            />
+          </Flex>
+
+          <Flex className={classes.flexItem}>
+            <Text className={classes.text}> Username </Text>
+            <TextInput 
+              className={classes.textInput}
+              key={form.key('username')}
+              {...form.getInputProps('username')}
+            />
+          </Flex>
+
+          <Flex className={classes.flexItem}>
+            <Text className={classes.text}> Email </Text>
+            <TextInput 
+              className={classes.textInput}
+              key={form.key('email')}
+              {...form.getInputProps('email')} 
+            />
+          </Flex>
+
+          <Flex className={classes.flexItem}>
+            <Text className={classes.text}> Password </Text>
+            <Button variant='default'> Change Password</Button>
+          </Flex>
+
+          <Flex justify='flex-end'>
+            <Button type="submit">
+              Save
+            </Button>
+          </Flex>
+        </form>
+
       </Flex>
 
       
